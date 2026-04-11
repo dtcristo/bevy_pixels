b6bf5d17acf550e7e93717b9c68a63b7b8b947a7
David Cristofaro <david@dtcristo.com>
2026-04-11 21:15:09 +1000

    PixelsWrapper Send+Sync on wasm32

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -6,3 +6,11 @@ use pixels::Pixels;
 pub struct PixelsWrapper {
     pub pixels: Pixels<'static>,
 }
+
+#[cfg(target_arch = "wasm32")]
+// Web builds run this integration on the browser's main thread, so `Pixels` never crosses thread
+// boundaries even though `wgpu` marks it as !Send/!Sync on wasm targets.
+unsafe impl Send for PixelsWrapper {}
+
+#[cfg(target_arch = "wasm32")]
+unsafe impl Sync for PixelsWrapper {}
